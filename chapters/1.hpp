8aa0b16134329cdacb16a628ed52ed5bcb74efbc
Kivattt <33596810+Kivattt@users.noreply.github.com>
2018-08-25 22:45:47 +0200

    Add files via upload

--- chapters/1.hpp
+++ chapters/1.hpp
@@ -4,60 +4,19 @@
 using std::string;
 using std::vector;
 
-extern unsigned chapterNum;
-
-template <typename T>
-bool in_range(const T val, const T low, const T high){
-	return val >= low && val <= high;
-}
-
-template <typename T>
-bool close(const T val1, const T val2, const double precision=1.15){
-	return in_range(val1, val2/precision, val2*precision);
-}
-
-template <typename T>
-T get_middle_elem(const vector <T> vec){
-	if (vec.empty()) return {};
-	const unsigned middleIdx = vec.size() / 2;
-	return vec[middleIdx];
-}
-
-template <typename T>
-void print_vec(const vector <T> vec){
-	if (vec.empty()) return;
-	std::cout << "{" << vec[0];
-	for (unsigned i = 1; i < vec.size(); i++)
-		std::cout << ", " << vec[i];
-	std::cout << "}\n";
-}
-
-double my_sqrt(const double n, const unsigned iterPrecision=500, const double closePrecision=1.05){
-	vector <double> validResults;
-
-	for (double i = 0; i < n; i += n/iterPrecision){
-		if (close(i*i, n, closePrecision))
-			validResults.push_back(i);
-	}
-
-	// Debugging
-	std::cout << "--------------------";
-	print_vec(validResults);
-	return get_middle_elem(validResults);
-
-	return 0;
-}
 /*
 void draw_title_screen_bg(){
 	
 }
 */
 exitcode chapter_1(){
-	std::cout << "my_sqrt testing\n";
+	std::cout << "Math::sqrt testing\n";
+
+	Math meth;
 
 	const vector <unsigned> vals{1,2,3,4,5,6,7,8,9,10};
 	for (auto i : vals)
-		std::cout << "my_sqrt(" << i << ", 1000, 1.01) = " << my_sqrt(i, 1000, 1.01) << '\n';
+		std::cout << "my_sqrt(" << i << ", 1000, 1.01) = " << meth.sqrt(i, 1000, 1.01) << '\n';
 
 	return success;
 }
