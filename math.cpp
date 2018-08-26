2f08191abeac9d4f7e673624feb7161c117e9ea1
Kivattt <33596810+Kivattt@users.noreply.github.com>
2018-08-26 16:50:20 +0200

    Add files via upload

--- math.cpp
+++ math.cpp
@@ -10,7 +10,7 @@ bool Math::in_range(const T val, const T low, const T high){
 }
 
 template <typename T>
-bool Math::close(const T val1, const T val2, const double precision){
+bool Math::close(const T val1, const T val2, const double long precision){
 	return in_range(val1, val2/precision, val2*precision);
 }
 
@@ -18,14 +18,16 @@ template <typename T>
 T Math::get_middle_elem(const vector <T> vec){
 	if (vec.empty()) return {};
 	return vec[vec.size()/2];
-	// Compiler complains about this:
+	//Compiler complains about this:
 	//return vec.empty() ? {} : vec[vec.size()/2];
 }
 
-double Math::sqrt(const double n, const unsigned iterPrecision, const double closePrecision){
+double long Math::sqrt(const double long n, const unsigned long long iterPrecision, const double long closePrecision){
+	if (n == 0) return 0;
+
 	vector <double> validResults;
 
-	for (double i = 0; i < n; i += n/iterPrecision){
+	for (double long i = 0; i < n; i += n/iterPrecision){
 		if (close(i*i, n, closePrecision))
 			validResults.push_back(i);
 	}
@@ -33,4 +35,28 @@ double Math::sqrt(const double n, const unsigned iterPrecision, const double clo
 	return get_middle_elem(validResults);
 
 	return 0;
+}
+
+unsigned long long Math::sqrt_results_size(const double long n, const unsigned long long iterPrecision, const double long closePrecision){
+	unsigned long long out = 0;
+	for (double long i = 0; i < n; i += n/iterPrecision)
+		out += close(i*i, n, closePrecision);
+
+	return out;
+}
+
+double long Math::accurate_sqrt(const double long n){
+	if (n == 0) return 0;
+
+	unsigned long long iterPrecision = 100000;
+	double long closePrecision = 1.01;
+
+	unsigned long long size = 0;
+
+	do {
+		size = sqrt_results_size(n, iterPrecision, closePrecision);
+		closePrecision /= 1.1;
+	} while (size != 1);
+
+	return sqrt(n, iterPrecision, closePrecision);
 }
---
+++ math.hpp
@@ -0,0 +1,24 @@
+#ifndef MATH_HPP
+#define MATH_HPP
+
+#include "includes.hpp"
+
+using std::string;
+using std::vector;
+
+struct Math{
+	Math(){std::cout.precision(15);}
+
+	double long sqrt(const double long, const unsigned long long=30000, const double long=1.005);
+	double long accurate_sqrt(const double long);
+	unsigned long long sqrt_results_size(const double long, const unsigned long long=30000, const double long=1.005);
+
+	template <typename T>	
+	T get_middle_elem(const vector <T>);
+	template <typename T>
+	bool close(const T, const T, const double long=1.05);
+	template <typename T>
+	bool in_range(const T, const T, const T);
+};
+
+#endif // MATH_HPP
