fcbea089b53fc6e439344284da6af0796397fa35
Kivattt <33596810+Kivattt@users.noreply.github.com>
2018-08-27 07:27:54 +0200

    Add files via upload

--- chapters/1.hpp
+++ chapters/1.hpp
@@ -9,11 +9,17 @@ void draw_title_screen_bg(){
 }
 */
 exitcode chapter_1(){
-	std::cout << "Math::sqrt testing\n" << std::flush;
+/*	std::cout << "Math::sqrt testing\n" << std::flush;
 
 	Math math(100);
 	for (unsigned i = 0; i < 100; i++)
 		std::cout << math.n_root(i) << ", " << std::flush;
+*/
+	Math math;
+	vector <double> vals{1,2,3,4,5,6,67,84,44,30,22,100};
+	math.print_vec(vals);
+	math.map(vals);
+	math.print_vec(vals);
 
 	return success;
 }
