370cccc1442d603829802c071a39ea6f36c156df
Kivattt <33596810+Kivattt@users.noreply.github.com>
2018-09-02 13:17:36 +0200

    Add files via upload
    Move all function definitions from math.cpp to math.hpp to acommodate for dumb template errors that I won't bother fixing right now.

--- math.hpp
+++ math.hpp
@@ -9,23 +9,71 @@ using std::vector;
 struct Math{
 	Math(const unsigned short numberPrecision=8){std::cout.precision(numberPrecision);}
 
-	// Default power in n_root* is 2 (square)
-	double long n_root(const double long, const unsigned long long=55000, const double long=1.001, const unsigned long long power=2);
-	unsigned long long n_root_result_size(const double long, const unsigned long long=55000, const double long=1.001, const unsigned long long=2);
-	double long pow(const double long, const unsigned long long=2);
 	template <typename T>
-	vector <T> map_copy(const vector <T>, const unsigned long long=1);
-	template <typename T>
-	void map(const vector <T> &, const unsigned long long=1);
+	bool in_range(const T val, const T low, const T high){
+		return val >= low && val <= high;
+	}
 
-	template <typename T>	
-	T get_middle_elem(const vector <T>);
+	double long pow(const double long n, const unsigned long long power=2){
+		double long out = n;
+		for (unsigned i = 1; i < power; i++)
+			out *= n;
+		return out;
+	}
+
+	// Default power in n_root* is 2 (square)
+	double long n_root(const double long n, const unsigned long long iterPrecision=55000, const double long closePrecision=1.001, const unsigned long long power=2){
+		if (n == 0) return 0;
+		
+		vector <double> validResults;
+		
+		for (double long i = n/iterPrecision; i < n; i += n/iterPrecision){
+			if (in_range(pow(i, power), n/closePrecision, n*closePrecision))
+				validResults.push_back(i);
+		}
+		
+		return validResults[validResults.size()/2];
+	}
+
+	unsigned long long n_root_result_size(const double long n, const unsigned long long iterPrecision=55000, const double long closePrecision=1.001, const unsigned long long power=2){
+		unsigned long long out = 0;
+		for (double long i = 0; i < n; i += n/iterPrecision)
+			out += in_range(pow(i, power), n/closePrecision, n*closePrecision);
+		
+		return out;
+	}
+
 	template <typename T>
-	bool in_range(const T, const T, const T);
+	void map(const vector <T> &vec, const unsigned long long highestVal=1){
+		const T highestVecVal = vec[vec.size()-1];
+		for (unsigned long long i = 0; i < vec.size(); i++){
+			vec[i] /= highestVecVal;
+			vec[i] *= highestVal;
+		}
+	}
+
 	template <typename T>
-	void print_vec(const vector <T>);
+	vector <T> map_copy(const vector <T> vec, const unsigned long long highestVal=1){
+		vector <T> tmp = vec;
+		map(tmp, highestVal);
+		return tmp;
+	}
+
+
 	template <typename T>
-	T Math::get_val(const vector <T>, const bool=true);
+	void print_vec(const vector <T> vec){
+		if (vec.empty()) return;
+
+		if (vec.size() == 1){
+			std::cout << '{' << vec[0] << '}';
+			return;
+		}
+
+		std::cout << '{' << vec[0];
+		for (unsigned i = 1; i < vec.size(); i++)
+			std::cout << ',' << vec[i];
+		std::cout << '}';
+	}
 };
 
 #endif // MATH_HPP
