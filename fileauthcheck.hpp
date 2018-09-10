5ccbf8c8d903cf7e18c3327c48ed17314c3b3f47
Kivattt <33596810+Kivattt@users.noreply.github.com>
2018-09-10 13:40:49 +0200

    Update fileauthcheck.hpp

--- fileauthcheck.hpp
+++ fileauthcheck.hpp
@@ -1,1 +1,18 @@
+#ifndef FILEAUTHCHECK_HPP
+#define FILEAUTHCHECK_HPP
 
+#include "includes.hpp"
+
+struct FileAuthenticator{
+	unsigned long long get_sum(const string filename){
+		std::ifstream
+	}
+
+
+
+	bool authentic(const string filename){
+		
+	}
+};
+
+#endif // FILEAUTHCHECK_HPP
