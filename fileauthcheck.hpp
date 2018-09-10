c486f53e4d320810460223776429c2b2751d9a8f
Kivattt <33596810+Kivattt@users.noreply.github.com>
2018-09-10 14:41:47 +0200

    Update fileauthcheck.hpp
    oh boy

--- fileauthcheck.hpp
+++ fileauthcheck.hpp
@@ -3,15 +3,44 @@
 
 #include "includes.hpp"
 
-struct FileAuthenticator{
-	unsigned long long get_sum(const string filename){
-		std::ifstream
+using std::string;
+using std::vector;
+
+// For checking if file is original without having to include entire file contents in source
+class FileAuthenticator{
+	const string filesPath = "bin/";
+	const string audioPath = filesPath + "audio/";
+	
+	// List of numbers to "authenticate" every file
+	// First number is size in bytes of file
+	// Second is the sum of every char in file
+	map <string, unsigned long long, unsigned long long> fNums{};
+	
+	map <string, unsigned long long, unsigned long long> audioFNums{{"bg.wav", REPLACETHISNUM, REPLACETHISNUMTOO},
+												{"illegal.wav", REPLACETHISNUM, REPLACETHISNUMTOO},
+												{"lsd.wav", REPLACETHISNUM, REPLACETHISNUMTOO},
+												{"panic!_at_the_mall.wav", REPLACETHISNUM, REPLACETHISNUMTOO},
+												{"un_subtle.wav", REPLACETHISNUM, REPLACETHISNUMTOO},
+												{"vague.wav", REPLACETHISNUM, REPLACETHISNUMTOO}};
+	for (auto &idx : audioFSizes)
+		idx.first = audioPath + idx.first; // Add audio path to every audio file
+	
+	public:
+	unsigned long long get_size(const string filename){
+		std::ifstream file(filename.c_str(), std::ifstream::in, std::ifstream::binary);
+		if (!file.is_open()) return error;
+		
+		file.seekg(0, std::ios::end);
+		unsigned long long fSize = file.tellg();
+		file.close();
+		
+		return fSize;
 	}
 
 
 
 	bool authentic(const string filename){
-		
+		if ()
 	}
 };
 
